0861746ef6995405aac6eacc2a1a3c06ec3e5538
milkyway002 <sea_bom_12@naver.com>
2018-07-12 23:55:48 +0900

    add clock base

---
+++ login page/next_page_JS.js
@@ -0,0 +1,5 @@
+function time () {
+    var time= new Date();
+    var day=time.getFullYear() + "-" + (time.getMonth()+1) + "-" + time.getDate(); 
+    document.getElementById(time.getFullYear());
+}
