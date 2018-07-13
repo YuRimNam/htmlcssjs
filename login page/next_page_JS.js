f40aba3c7456a5ef33d2432365dc0fd069be28f4
milkyway002 <sea_bom_12@naver.com>
2018-07-13 20:20:57 +0900

    update clock

--- login page/next_page_JS.js
+++ login page/next_page_JS.js
@@ -1,5 +1,16 @@
 function time () {
     var time= new Date();
-    var day=time.getFullYear() + "-" + (time.getMonth()+1) + "-" + time.getDate(); 
-    document.getElementById(time.getFullYear());
+    var ampm= 'AM';
+    var hour=addZeros(timeDate.getHours(), 2);
+    var min=addZeros(timeDate.getMinutes(), 2);
+    var sec=addZeros(timeDate.getSeconds(), 2);
+
+    if (hour>=12)
+    {
+        ampm='PM';
+        hour=addZeros(hour-12, 2);
+    }
+
+    settimeout("time()", 1000);
+    document.getElementById('clock').innerHTML = h+':'+m+':'+s;
 }
