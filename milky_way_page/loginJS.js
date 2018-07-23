946c69f64c1ba52fb91f725dcd938037b5baeca3
milkyway002 <sea_bom_12@naver.com>
2018-07-23 21:45:36 +0900

    update login

--- milky_way_page/loginJS.js
+++ milky_way_page/loginJS.js
@@ -1,14 +1,10 @@
 function next_page ()
 {
-    var pk=$('#id').val();
-    var ps=$('#password').val();
+    var pk=document.getElementById('id').value;
+    var ps=document.getElementById('password').value;
 
     if (pk=='sea_bom_12' && ps=='gpfla123')
-    {
         location.href="./next_page_HTML.html";
-    }
-    else
-    {
+    else if(pk!='sea_bom_12' && ps!='gpfla123')
         alert('잘못 입력하셨습니다. 다시 입력하세요.');
-    }
 }
