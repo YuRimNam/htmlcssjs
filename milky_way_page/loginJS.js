75e2e6db2b3f3bdb0e8ba130f7acbd96695b36e7
milkyway002 <sea_bom_12@naver.com>
2018-08-07 15:50:58 +0900

    add funtion code

--- milky_way_page/loginJS.js
+++ milky_way_page/loginJS.js
@@ -3,8 +3,12 @@ function next_page ()
     var pk=document.getElementById('id').value;
     var ps=document.getElementById('password').value;
 
-    if (pk=='sea_bom_12' && ps=='gpfla123')
+    if (pk=='sea_bom_12' && ps=='gpfladbfla1')
         location.href="./next_page_HTML.html";
+    else if(pk=='')
+        alert('아이디를 입력하세요.');
+    else if(ps=='')
+        alert('패스워드를 입력하세요.');
     else if(pk!='sea_bom_12' && ps!='gpfla123')
         alert('잘못 입력하셨습니다. 다시 입력하세요.');
 }
